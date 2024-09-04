51af68f2e9aff39afe7061622bb21cdf2d5134fe
VinceBmmrt <vincebommert75@gmail.com>
2024-09-04 18:10:53 +0200

    Add initial dashboard page with: - Revenue chart component - Latest invoices component - Summary cards for collected, pending, total invoices, and total customers data

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,44 +1,47 @@
+import { lusitana } from "@/app/ui/fonts";
 import {
   BanknotesIcon,
   ClockIcon,
-  UserGroupIcon,
   InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
-
+  UserGroupIcon,
+} from "@heroicons/react/24/outline";
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
   pending: ClockIcon,
   invoices: InboxIcon,
 };
-
-export default async function CardWrapper() {
+type CardWrapperProps = {
+  totalPaidInvoices: number;
+  numberOfInvoices: number;
+  totalPendingInvoices: number;
+  numberOfCustomers: number;
+};
+export default async function CardWrapper({
+  totalPaidInvoices,
+  numberOfInvoices,
+  totalPendingInvoices,
+  numberOfCustomers,
+}: CardWrapperProps) {
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
-
-export function Card({
-  title,
-  value,
-  type,
-}: {
+type CardProps = {
   title: string;
   value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
-}) {
+  type: "invoices" | "customers" | "pending" | "collected";
+};
+export function Card({ title, value, type }: CardProps) {
   const Icon = iconMap[type];
 
   return (
